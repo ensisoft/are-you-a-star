405392da0151a715a6def0e8d192adbefec6fb17
Sami Väisänen <samiv@ensisoft.com>
2015-08-20 21:52:20 +0800

    Refactored the php code to precede the html

--- yes.php
+++ yes.php
@@ -1,3 +1,92 @@
+<?php
+    include_once("database.php");
+
+    $err='';
+    $msg='';
+
+    function sql_string($str)
+    {
+        if (isset($str))
+            return "'" . mysql_real_escape_string(strip_tags($str)) . "'";
+        return $str;
+    }
+
+    function get_visitor_host()
+    {
+        $ip = getenv("HTTP_CLIENT_IP");
+        if (strlen($ip) == 0)
+        {
+            $ip = getenv("HTTP_X_FORWARDED_FOR");
+            if (strlen($ip) == 0)
+                $ip = getenv("REMOTE_ADDR");
+        }
+        if (strlen($ip))
+            return gethostbyaddr($ip);
+        return "unknown";                    
+    }
+
+    function check_repository($repository)
+    {
+        $ret = preg_match('#^https?://github\.com/[a-z]+/[a-z]+\.git#', $repository);
+        if ($ret == 0)
+            return 0;
+        $headers = @get_headers($repository);
+
+        foreach ($headers as $header) {
+            if (strcasecmp($header, "HTTP/1.1 404 Not Found") == 0)
+                return 0;
+        }
+        return 1;
+    }
+
+    function oops($msg)
+    {
+        global $err;
+        $err = $err . $msg;
+    }
+
+    $repository = $_REQUEST['repository'];
+
+    if (isset($repository))
+    {
+        if (check_repository($repository) == 0) {
+            oops("$repository is not a valid GitHub repository... :(");
+        }
+        else
+        {
+            $repository = sql_string($repository);
+            $visitor    = sql_string(get_visitor_host());
+
+            $insert = "INSERT INTO stars (visitor, repository) VALUES ($visitor, $repository)";
+            @mysql_query($insert, $db);
+
+            // http://dev.mysql.com/doc/refman/5.5/en/error-messages-server.html
+            // 1062 for duplicate entry.
+            if (mysql_errno() != 1062) {
+                oops("ooops the database has died.");
+            } else {
+                $msg = $msg . "<p>YES, YOU ARE A STAR!<br>" .
+                "CONGRATULATIONS :)<br><br><br></p>";
+               
+                $msg = $msg . "<img src=\"star.png\">";
+            }
+        }
+    }
+
+    $select  = "SELECT repository FROM stars ORDER BY RAND() LIMIT 1";
+    $records = mysql_query($select, $db);
+    if (mysql_errno()) {
+        oops("oops the database has died...");
+    } else {
+        $row = mysql_fetch_array($records);
+        $url = $row['repository'];
+
+        $msg = $msg . "<br><br>Now.. why don't you take a few minutes and visit <br><br>" .
+            "<a href=\"$url\">$url</a>";
+    }
+    @mysql_close($db);
+?>
+
 <!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN">
 <html>
     <head>
@@ -8,79 +97,9 @@
     <body>
         <div class="content">
             <?php
-                include("database.php");
-
-                function sql_string($str)
-                {
-                    if (isset($str))
-                        return "'" . mysql_real_escape_string(strip_tags($str)) . "'";
-                    return $str;
-                }
-                function get_visitor_host()
-                {
-                    $ip = getenv("HTTP_CLIENT_IP");
-                    if (strlen($ip) == 0)
-                    {
-                        $ip = getenv("HTTP_X_FORWARDED_FOR");
-                        if (strlen($ip) == 0)
-                            $ip = getenv("REMOTE_ADDR");
-                    }
-                    if (strlen($ip))
-                        return gethostbyaddr($ip);
-                    return "unknown";                    
-                }
-
-                function check_repository($repository)
-                {
-                    $ret = preg_match('#^https?://github\.com/[a-z]+/[a-z]+\.git#', $repository);
-                    if ($ret == 0)
-                        return 0;
-                    $headers = @get_headers($repository);
-
-                    foreach ($headers as $header) {
-                        if (strcasecmp($header, "HTTP/1.1 404 Not Found") == 0)
-                            return 0;
-                    }
-                    return 1;
-                }
-
-                $repository = $_REQUEST['repository'];
-
-
-                if (isset($repository))
-                {
-                    if (check_repository($repository) == 0)
-                        die("$repository is not a valid GitHub repository... :(");
-
-                    $repository = sql_string($repository);
-                    $visitor    = sql_string(get_visitor_host());
-
-                    $insert = "INSERT INTO stars (visitor, repository) VALUES ($visitor, $repository)";
-                    mysql_query($insert, $db);
-
-                    // http://dev.mysql.com/doc/refman/5.5/en/error-messages-server.html
-                    // 1062 for duplicate entry.
-                    if (mysql_errno() != 1062)
-                      die("ooops the database has died.");
-
-                    echo("<p>YES, YOU ARE A STAR!<br>" .
-                         "CONGRATULATIONS :)<br><br><br></p>");
-
-                    echo("<img src=\"star.png\">");
-                }
-
-                $select  = "SELECT repository FROM stars ORDER BY RAND() LIMIT 1";
-                $records = mysql_query($select, $db) or
-                  die("oops the database has died...");
-
-                $row = mysql_fetch_array($records);
-                $url = $row['repository'];
-
-                echo("<br><br>Now.. why don't you take a few minutes and visit <br><br>" .
-                    "<a href=\"$url\">$url</a>");
-
-                @mysql_close($db);
-            ?>        
+              if ($err) { echo $err; }
+              else if ($msg) { echo $msg; }
+            ?>
         </div>
 
         <div style="width:100%">
